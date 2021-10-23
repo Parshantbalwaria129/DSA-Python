e49de1e1f6baf5c3d2a633476b9a309df9869e61
Suman Bishnoi <86405366+SumanBishnoi@users.noreply.github.com>
2021-10-23 14:37:11 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,37 @@
+#using math module
+import math
+a= math.log(10)
+print(a)
+
+a= math.log(10)
+print("a= ",a)
+
+b= math.log10(10)
+print("b= ",b)
+
+c= math.exp(2)
+print("c= ",c)
+
+d= math.pow(4,3)
+print("d= ",d)
+
+e= math.sqrt(16)
+print("e= ",e)
+
+f= math.degrees(2*math.pi)
+print("f= ",f)
+
+g= math.pi 
+print("g= ",g)
+
+i= math.radians(180)
+print("i= ",i)
+
+j=math.sin(math.radians(60))
+print("j= ",j)
+
+k= math.ceil(4.76)
+print("k= ",k)
+
+l= math.floor(9.78)
+print("l= ",l)
